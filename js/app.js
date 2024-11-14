aae649a905f9401d2a76301df70a08947ebe6a67
Ashley M <ashleybztoofly@yahoo.com>
2024-11-13 19:20:12 -0600

    added nav bar

--- js/app.js
+++ js/app.js
@@ -24,13 +24,13 @@
 */
 
 //--global variables??
-const navbar__menu = navMenu
-const navbar__list =  navList
+const navbar__menu = document.getElementsByClassName('.navbar__menu');
+const navbar__list = document.getElementById('#navbar__list');
 
 
 
 
-    
+   
 const sections = ["Section 1", "Section 2", "Section 3", "Section 4"]
 for (let i=0; i < sections.length; i++) {
     let section = sections[i];
@@ -39,7 +39,7 @@ for (let i=0; i < sections.length; i++) {
     document.getElementById('navbar__list').appendChild(ul);
 }
    //-- console.log(sections);
-
+/* 
 
 
 
@@ -58,14 +58,21 @@ for (let i=0; i < sections.length; i++) {
  * 
 */
 
-// build the nav
-document.getElementsByClassName('.navbar__menu');
-const navMenu = $0;
-navMenu.style.height = '50px';
-document.getElementsById('navbar__list');
-const navList = $0;
 
 /*
+// build out the nav bar //worked
+const navMenu = document.getElementsByClassName('.navbar__menu');
+navMenu.style.height = '50px';
+
+//--makes nav list section 1 //worked in console
+const navList = document.getElementById('#navbar__list');
+const navList = document.createElement('li');
+navList.innerHTML = '<strong> Section 1 <strong>';
+navList.textContent = 'Section 1';
+
+list.append(listItem)
+
+
 //make list items (do not add to HTML)
 <ul id="navbar__list">
     <li><a class="menu__link" href="#section1">Section 1</a></li>
@@ -74,18 +81,30 @@ const navList = $0;
     <li><a class="menu__link" href="#section4">Section 4</a></li>
 </ul>
 
+//--test navigation buttons
+const navListItems = [
+    {href: "data-nav=Section1", text: 'Section 1'}
+    {href: "data-nav=Section2", text: 'Section 2'}
+    {href: "data-nav=Section3", text: 'Section 3'}
+    {href: "data-nav=Section4", text: 'Section 4'}
+];
+*/
+//--applied changes to console worked
+const navList = document.getElementById('#navbar__list');
+navList.style.color = 'black';
+navList.style.height = '50px';
+navList.style.fontSize = '2em';
+navList.style.padding = '10px';
 
 
 
-
-
-const mainHeading = document.querySelector('h1');
-
-mainHeading.style.cssText = 'color: blue; background-color: orange; font-size: 3.5em';
+//--did not work
+const navList = document.querySelector('navbar__list');
+navList.style.cssText = 'color: black; height: 50px; font-size: 2em; padding: 10px';
 
 // Add class 'active' to section when near top of viewport
 
-//Tip: detect the element location relatice to the viewport using .getBoundingClientRect()
+//--Tip: detect the element location relatice to the viewport using .getBoundingClientRect()
 function makeActive(){
     for (cons section of sections) {
         const box = section.getBoundingClientRect();
@@ -107,13 +126,16 @@ scroll()
 scrollBy()
 scrollIntoView()
 are acceptable
-/**
+
+
+/*
  * End Main Functions
  * Begin Events
  * 
 */
-//--test event listener code
 
+/*
+//--test event listener code
 document.addEventListener('click', function() {
     // select the element
     const sectionTabs = document.querySelector('navbar__list');
@@ -122,7 +144,16 @@ document.addEventListener('click', function() {
   	sectionTabs.style.backgroundColor = 'red';
 })
 
-use prevent default()
+//--use prevent default()
+//test
+submit.addEventListener(“click”, function(event){
+let value = textArea.value;
+let li = document.createElement(“li”);
+li.textContent = value;
+ul.appendChild(li);
+event.preventDefault();
+});
+
 
 // Build menu 
 
@@ -130,9 +161,11 @@ use prevent default()
 
 // Set sections as active
 // Make sections active document.addEventListener("scroll", function() { makeActive();});***
-* Set CSS class active state when the element is in the viewport.
-* *Hint: see this* <a href="https://knowledge.udacity.com/questions/85408" target="_blank">*Knowledge post*</a> *to implement this functionality.*
 
-<br data-md>
+//--*Set CSS class active state when the element is in the viewport.
+//--*Hint: see this* <a href="https://knowledge.udacity.com/questions/85408" target="_blank">*Knowledge post*</a> *to implement this functionality.*
 
-update readme
+//--<br data-md>
+
+//--update readme
+*/
